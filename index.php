21f39f5f417f8f8aaa5ff15c07b344a38e6ecbde
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-12-14 23:17:01 +0100

    Modifica grafica e aggiunge form statico La select non è funzionante

--- index.php
+++ index.php
@@ -52,7 +52,6 @@
         ],
 
     ];
-
 ?>
 
 <!DOCTYPE html>
@@ -62,37 +61,50 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+    <!-- Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- /Bootstrap CDN -->
 </head>
 <body>
-    <h1>PHP Hotel</h1>
-    <h2>Tutti gli hotels:</h2>
-    <table class="table">
-        <?php foreach( $hotels as $hotel) { ?>
-            <thead class="table-light text-center">
-                <tr>
-                    <th scope="col" colspan="2"><?php echo $hotel['name'] ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">Descrizione:</th>
-                    <td><?php echo $hotel['description'] ?></td>
-                </tr>
-                <tr>
-                    <th scope="row">Parcheggio:</th>
-                    <td><?php echo $hotel['parking'] ? 'Sì' : 'No' ?></td>
-                </tr>
-                <tr>
-                    <th scope="row">Voto:</th>
-                    <td><?php echo $hotel['vote'] ?></td>
-                </tr>
-                <tr>
-                    <th scope="row">Distanza dal centro:</th>
-                    <td><?php echo $hotel['distance_to_center'] .' km' ?></td>
-                </tr>
-            </tbody>
-        <?php } ?>
-    </table>
+    <div class="container">
+        <h1 class="text-center fw-semibold my-3">PHP Hotel</h1>
+        <h3 class="my-3">I nostri hotels:</h3>
+        <form action="index.php" method="GET" class="mb-3">
+            <label for="parking" class="mb-2">Filtra per disponibilità parcheggio:</label>
+            <select name="parking" id="parking" class="form-select w-auto" aria-label="Default select example">
+                <option selected>Nessun filtro</option>
+                <option value="true">Sì</option>
+                <option value="false">NO</option>
+            </select>
+        </form>
+        <table class="table">
+            <?php foreach( $hotels as $hotel) { ?>
+                <thead class="table-light text-center">
+                    <tr>
+                        <th scope="col" colspan="2" class="col-4 fs-5"><?php echo $hotel['name'] ?></th>
+                    </tr>
+                </thead>
+                <tbody class="mb-4">
+                    <tr>
+                        <th scope="row">Descrizione:</th>
+                        <td><?php echo $hotel['description'] ?></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Parcheggio:</th>
+                        <td><?php echo $hotel['parking'] ? 'Sì' : 'No' ?></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Voto:</th>
+                        <td><?php echo $hotel['vote'] ?></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Distanza dal centro:</th>
+                        <td><?php echo $hotel['distance_to_center'] .' km' ?></td>
+                    </tr>
+                </tbody>
+            <?php } ?>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
